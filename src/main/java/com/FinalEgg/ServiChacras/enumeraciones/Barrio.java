d0be4b98ee3e0d741a47ae52e767b40654a308f2
Psaycher <133926931+Psaycher@users.noreply.github.com>
2024-06-22 22:00:31 -0300

    "Updated Barrio enum to use underscores instead of camel case, and refactored UsuarioServicio and registro.html to match the changes."

--- src/main/java/com/FinalEgg/ServiChacras/enumeraciones/Barrio.java
+++ src/main/java/com/FinalEgg/ServiChacras/enumeraciones/Barrio.java
@@ -1,8 +1,8 @@
 package com.FinalEgg.ServiChacras.enumeraciones;
 
 public enum Barrio {
-    BARRIO1Ruta2km69,
-    BARRIO2Ruta2km75,
-    BARRIO3Ruta13km86,
+    BARRIO1_Ruta2km69,
+    BARRIO2_Ruta2km75,
+    BARRIO3_Ruta13km86,
     FORANEO;
 }
--- src/main/java/com/FinalEgg/ServiChacras/servicios/UsuarioServicio.java
+++ src/main/java/com/FinalEgg/ServiChacras/servicios/UsuarioServicio.java
@@ -53,9 +53,9 @@ public class UsuarioServicio implements UserDetailsService {
         usuario.setPassword(new BCryptPasswordEncoder().encode(password));
 
         switch (barrio) {
-            case 1 -> { usuario.setBarrio(Barrio.BARRIO1Ruta2km69); }
-            case 2 -> { usuario.setBarrio(Barrio.BARRIO2Ruta2km75); }
-            case 3 -> { usuario.setBarrio(Barrio.BARRIO3Ruta13km86); }
+            case 1 -> { usuario.setBarrio(Barrio.BARRIO1_Ruta2km69); }
+            case 2 -> { usuario.setBarrio(Barrio.BARRIO2_Ruta2km75); }
+            case 3 -> { usuario.setBarrio(Barrio.BARRIO3_Ruta13km86); }
             default -> { usuario.setBarrio(Barrio.FORANEO); }
         }
 
@@ -129,9 +129,9 @@ public class UsuarioServicio implements UserDetailsService {
 
             if (barrio != null) {
                 switch (barrio) {
-                    case 1 -> { usuario.setBarrio(Barrio.BARRIO1Ruta2km69); }
-                    case 2 -> { usuario.setBarrio(Barrio.BARRIO2Ruta2km75); }
-                    case 3 -> { usuario.setBarrio(Barrio.BARRIO3Ruta13km86); }
+                    case 1 -> { usuario.setBarrio(Barrio.BARRIO1_Ruta2km69); }
+                    case 2 -> { usuario.setBarrio(Barrio.BARRIO2_Ruta2km75); }
+                    case 3 -> { usuario.setBarrio(Barrio.BARRIO3_Ruta13km86); }
                     default -> { usuario.setBarrio(Barrio.FORANEO); }
                 }
             } else { usuario.setBarrio(Barrio.FORANEO); }
