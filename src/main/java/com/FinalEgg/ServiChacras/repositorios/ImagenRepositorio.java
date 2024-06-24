583a7250658c7e10ae9b68f20992f4a2e01ebb71
Psaycher <psaycher@gmail.com>
2024-06-24 01:18:03 -0300

    Delete src directory

--- src/main/java/com/FinalEgg/ServiChacras/repositorios/ImagenRepositorio.java
+++
@@ -1,8 +0,0 @@
-package com.FinalEgg.ServiChacras.repositorios;
-
-import org.springframework.stereotype.Repository;
-import com.FinalEgg.ServiChacras.entidades.Imagen;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface ImagenRepositorio extends JpaRepository<Imagen, String> {}
